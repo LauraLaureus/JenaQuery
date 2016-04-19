61f27ea63330e1861f0a4c602af824ce8969f924
Laura <l4depende@gmail.com>
2016-04-19 11:36:02 +0100

    Code Query

--- src/jenaquery/JenaQuery.java
+++ src/jenaquery/JenaQuery.java
@@ -1,9 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jenaquery;
+
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.util.PrintUtil;
 
 /**
  *
@@ -11,11 +17,32 @@ package jenaquery;
  */
 public class JenaQuery {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        String ns = "urn:si2/ej3/";
+
+        Model model = RDFDataMgr.loadModel("datos.ttl");
+
+        String query = "PREFIX foaf:<http://xmlns.com/foaf/0.1/>\n"
+                + "\n"
+                + "SELECT ?nombre ?nombreamigo\n"
+                + "WHERE {\n"
+                + "   ?v foaf:name ?nombre .\n"
+                + "   ?v foaf:knows ?amigo .\n"
+                + "   ?amigo foaf:name ?nombreamigo .\n"
+                + "}";
+        
+        Query executableQuery = QueryFactory.create(query);
+        QueryExecution excecution = QueryExecutionFactory.create(executableQuery,model);
+        
+        ResultSet results = excecution.execSelect();
+        for (; results.hasNext();) {
+            QuerySolution sol = results.next();
+            RDFNode x = sol.get("?nombre");
+            System.out.println("--->" + PrintUtil.print(x));
+        }
+        
+        excecution.close();
     }
-    
+
 }
